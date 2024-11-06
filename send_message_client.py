3ca4af90df874a0910e40e763ce0eb630d8ec63b
Mark Tereshchenko <astr6.6.6@yandex.ru>
2024-11-06 17:21:03 +0300

    Add CLI options to sender script.

--- send_message_client.py
+++ send_message_client.py
@@ -1,62 +1,109 @@
 import asyncio
+import json
+import os
+
 import configargparse
 from environs import Env
-from utils import get_logger
-import json
+
+from utils import get_logger, update_env_file
 
 
-async def send_message_chat_client(host, port, token):
-    # Открыли подключение
+async def send_message_chat_client(host, port, token, message):
     reader, writer = await asyncio.open_connection(host, port)
-    logger = await get_logger('sender')
 
-    # Вывели стартовое сообщение
-    start_message = await reader.readline()
-    print(start_message.decode().strip())
-
-    # Отправили ключ
-    writer.write(f'{token}\n'.encode())
-    await writer.drain()
-  
-    # Обработали кривой ключ
-    received_message = await reader.readline()
-    message = received_message.decode().strip()
-    if json.loads(message) is None:
-        message = 'Неизвестный токен. Проверьте его или зарегистрируйте заново.'
-        print(message)
-        logger.error(message)
-        return
-
-    received_message = await reader.readline()
-    message = received_message.decode().strip()
-    print(message)
-
-    # Ожидание сообщений
     try:
+        logger = await get_logger("sender")
+        start_message = await reader.readline()
+        print(start_message.decode().strip())
+
+        if token:
+            await authorize(writer, reader, token, logger)
+
+        else:
+            token = await register(writer, reader, token, logger)
+            await update_env_file("TOKEN", token)
+            os.system("clear")
+            await send_message_chat_client(host, port, token)
+
+        received_message = await reader.readline()
+        print(received_message.decode().strip())
+
         while True:
-            message = input('Введите сообщение: ') + '\n\n'
-            writer.write(message.encode())
-            await writer.drain()
+            if message:
+                await send_message(writer, message)
+            message = input("Введите сообщение: ") + "\n\n"
     except KeyboardInterrupt:
         logger.debug("\nОтключение от сервера.")
     finally:
         writer.close()
         await writer.wait_closed()
 
+
+async def send_message(writer, message):
+    writer.write(message.encode())
+    await writer.drain()
+
+
+async def authorize(writer, reader, token, logger):
+    writer.write(f"{token}\n".encode())
+    await writer.drain()
+
+    received_message = await reader.readline()
+    message = received_message.decode().strip()
+
+    if json.loads(message) is None:
+        message = "Неизвестный токен. Проверьте его или зарегистрируйте заново."
+        print(message)
+        logger.error(message)
+
+        raise KeyboardInterrupt
+
+
+async def register(writer, reader, token, logger):
+    writer.write(f"{token}\n".encode())
+    await writer.drain()
+
+    received_message = await reader.readline()
+    message = received_message.decode().strip()
+    print(message)
+
+    username = input(
+        "Для регистрации введите имя пользователя или нажмите Enter для автоматической генерации: "
+    )
+    writer.write(f"{username}\n".encode())
+    await writer.drain()
+
+    received_message = await reader.readline()
+    message = json.loads(received_message.decode().strip())
+    print(message)
+    token = message["account_hash"]
+    print(f"Регистрация прошла успешно. Через 5 секунд консоль будет обновлена.")
+
+    await asyncio.sleep(5)
+
+    return token
+
+
 def main():
     env = Env()
     env.read_env()
     host = env.str("HOST")
     port = env.int("SEND_PORT")
-    token = env.str("TOKEN")
+    token = env.str("TOKEN", "")
 
     p = configargparse.ArgParser()
     p.add("-H", "--host", help="chat host", default=host, required=False)
     p.add("-P", "--port", help="chat port", default=port, required=False)
-
+    p.add("-T", "--token", help="chat token", default=token, required=False)
+    p.add("-M", "--message", help="chat message", required=True)
     options = p.parse_args()
 
-    asyncio.run(send_message_chat_client(options.host, options.port, token))
+    asyncio.run(
+        send_message_chat_client(
+            options.host, options.port, token, f"{options.message}\n\n"
+        )
+    )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -1,12 +1,28 @@
 import logging
 
+import aiofiles
+
 
 async def get_logger(logger_status):
     logging.basicConfig(
-        filename='chat_client.log', 
+        filename="chat_client.log",
         level=logging.DEBUG,
-        format='%(levelname)s:%(name)s:%(message)s'
+        format="%(levelname)s:%(name)s:%(message)s",
     )
     logger = logging.getLogger(logger_status)
 
     return logger
+
+
+async def update_env_file(key, value):
+    lines = []
+
+    async with aiofiles.open(".env", mode="r") as file:
+        async for line in file:
+            if line.startswith(f"{key}="):
+                lines.append(f"{key}={value}\n")
+            else:
+                lines.append(line)
+
+    async with aiofiles.open(".env", mode="w") as file:
+        await file.writelines(lines)
