a72c77f39d07f9785272a81f5c8866950b22eaf5
Mark Tereshchenko <astr6.6.6@yandex.ru>
2024-11-07 13:50:09 +0300

    Add block try/except/finally for receive_message_chat_client

--- read_message_client.py
+++ read_message_client.py
@@ -6,22 +6,28 @@ import aiofiles
 import configargparse
 from aiofiles.os import wrap
 from environs import Env
+from utils import get_logger
 
 
 async def receive_message_chat_client(host, port, history_filepath):
-    reader, _ = await asyncio.open_connection(host, port)
+    reader, writer = await asyncio.open_connection(host, port)
     write_stdout = wrap(sys.stdout.write)
+    logger = await get_logger('receiver')
+    try:
+        while True:
+            received_message = await reader.readuntil()
+            now = datetime.now()
 
-    while True:
-        received_message = await reader.readuntil()
-        now = datetime.now()
+            async with aiofiles.open(history_filepath, mode="a") as handle:
+                message = f'[{now.strftime("%d.%m.%Y %H:%M")}] {received_message.decode()}'
 
-        async with aiofiles.open(history_filepath, mode="a") as handle:
-            message = f'[{now.strftime("%d.%m.%Y %H:%M")}] {received_message.decode()}'
-
-            await write_stdout(message)
-            await handle.write(message)
-
+                await write_stdout(message)
+                await handle.write(message)
+    except KeyboardInterrupt:
+        logger.debug("\nОтключение от сервера.")
+    finally:
+        writer.close()
+        await writer.wait_closed()
 
 def main():
     env = Env()
