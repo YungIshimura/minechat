5faad01a33f65627a27d7e482e7686590cd28a5d
Mark Tereshchenko <astr6.6.6@yandex.ru>
2024-11-20 19:26:58 +0300

    Добавил gui для чата

---
+++ main.py
@@ -0,0 +1,169 @@
+import asyncio
+import json
+import socket
+from datetime import datetime
+from tkinter import TclError, messagebox
+from typing import Any, Dict
+
+import anyio
+from async_timeout import timeout
+from environs import Env
+
+import gui
+from exceptions import InvalidToken
+from utils import (
+    ConnectionSource, ConnectionStatus, 
+    get_queues,save_message, 
+    send_message, update_env_file,
+    update_messages, watch_for_connection
+)
+
+
+async def read_msgs(host: str, port: int, queues: Dict[str, asyncio.Queue]) -> None:
+    history_queue = asyncio.Queue()
+    status_updates_queue = queues['status_updates_queue']
+    watchdog_queue = queues['watchdog_queue']
+    messages_queue = queues['messages_queue']
+
+    while True:
+        try:
+            reader, _ = await asyncio.open_connection(host, port)
+            try:
+                while True:
+                    await ping_server(reader, gui.ReadConnectionStateChanged, status_updates_queue)
+
+                    received_message = await reader.readuntil()
+                    status_updates_queue.put_nowait(gui.ReadConnectionStateChanged.ESTABLISHED)
+                    await watch_for_connection(watchdog_queue, ConnectionStatus.ALIVE, ConnectionSource.NEW_MESSAGE)
+
+                    now = datetime.now()
+                    message = f'[{now.strftime("%d.%m.%Y %H:%M")}] {received_message.decode()}'
+                    messages_queue.put_nowait(message)
+
+                    await save_message(history_queue, message)
+            except asyncio.TimeoutError:
+                await watch_for_connection(watchdog_queue, ConnectionStatus.ALIVE, ConnectionSource.TIMEOUT)
+                status_updates_queue.put_nowait(gui.ReadConnectionStateChanged.CLOSED)
+        except socket.gaierror:
+            status_updates_queue.put_nowait(gui.ReadConnectionStateChanged.INITIATED)
+
+
+async def send_msgs(host: str, port: int, queues: Dict[str, asyncio.Queue], token: str) -> None:
+    is_auth = False
+    status_updates_queue = queues['status_updates_queue']
+    watchdog_queue = queues['watchdog_queue']
+    sending_queue = queues['sending_queue']
+
+    while True:
+        try:
+            reader, writer = await asyncio.open_connection(host, port)
+            try:
+                while True:
+                    if not is_auth:
+                        await watch_for_connection(watchdog_queue, ConnectionStatus.ALIVE, ConnectionSource.BEFORE_AUTH)
+                        nickname = await authorize(reader, writer,  token)
+                        await ping_server(reader, gui.SendingConnectionStateChanged, status_updates_queue)
+                        await watch_for_connection(watchdog_queue, ConnectionStatus.ALIVE, ConnectionSource.AUTH)
+                        nickname_received = gui.NicknameReceived(nickname)
+                        status_updates_queue.put_nowait(nickname_received)
+                        is_auth = True
+
+                    status_updates_queue.put_nowait(gui.SendingConnectionStateChanged.ESTABLISHED)
+
+                    msg = await sending_queue.get()
+                    if msg:
+                        await send_message(writer, f'{msg}\n\n')
+                        await watch_for_connection(watchdog_queue, ConnectionStatus.ALIVE, ConnectionSource.MESSAGE_SENT)
+            except asyncio.TimeoutError:
+                await watch_for_connection(watchdog_queue, ConnectionStatus.ALIVE, ConnectionSource.TIMEOUT)
+                status_updates_queue.put_nowait(gui.SendingConnectionStateChanged.CLOSED)
+                is_auth = False
+            except InvalidToken:
+                is_auth = False
+                break
+        except socket.gaierror:
+            is_auth = False
+            status_updates_queue.put_nowait(gui.SendingConnectionStateChanged.INITIATED)
+
+
+async def ping_server(reader: asyncio.StreamReader, gui_class: Any, status_updates_queue: asyncio.Queue) -> bool:
+    start_ping_time = 0
+
+    while start_ping_time < 1800:
+        try:
+            async with timeout(15) as cm:
+                received_message = await reader.readline()
+                if received_message:
+                    return True
+                cm.expired()
+        except asyncio.TimeoutError:
+            start_ping_time += 15
+            status_updates_queue.put_nowait(gui_class.CLOSED)
+
+    raise socket.gaierror
+
+
+async def authorize(reader: asyncio.StreamReader, writer: asyncio.StreamWriter, token: str) -> str:
+    try:
+        if token:
+            writer.write(f"{token}\n".encode())
+            await writer.drain()
+
+            for _ in range(2):
+                received_message = await reader.readline()
+                message = received_message.decode().strip()
+
+        else:
+            message = await register(reader, writer)
+
+        message_json = json.loads(message)
+
+        if message_json is None:
+            raise InvalidToken
+
+        await update_env_file('TOKEN', message_json['account_hash'])
+        return message_json['nickname']
+
+    except InvalidToken:
+        messagebox.showinfo("Неверный токен", "Проверьте токен, сервер его не узнал")
+
+
+async def register(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
+    nickname = gui.register_nickname()
+    writer.write(f"\n".encode())
+    await writer.drain()
+    for _ in range(2):
+        received_message = await reader.readline()
+        message = received_message.decode().strip()
+
+    writer.write(f"{nickname}\n".encode())
+    await writer.drain()
+
+    received_message = await reader.readline()
+    message = received_message.decode().strip()
+
+    return message
+
+
+async def main() -> None:
+    queues = await get_queues()
+
+    env = Env()
+    env.read_env()
+    host = env.str("HOST")
+    read_port = env.int("READ_PORT")
+    send_port = env.int("SEND_PORT")
+    token = env.str("TOKEN", "")
+    
+    async with anyio.create_task_group() as tg:
+        tg.start_soon(update_messages, queues)
+        tg.start_soon(read_msgs, host, read_port, queues)
+        tg.start_soon(send_msgs, host, send_port, queues, token)
+        tg.start_soon(gui.draw, queues)
+    
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except* (TclError, KeyboardInterrupt):
+        pass
--- utils.py
+++ utils.py
@@ -1,20 +1,37 @@
+import asyncio
+import enum
 import logging
+import time
+from typing import Dict
 
 import aiofiles
 
 
-async def get_logger(logger_status):
+class ConnectionStatus(enum.Enum):
+    ALIVE = "Connection is alive"
+    DEAD = "Connection is dead"
+
+
+class ConnectionSource(enum.Enum):
+    NEW_MESSAGE = "Source: New message in chat"
+    MESSAGE_SENT = "Source: Message sent"
+    TIMEOUT = "Source: 15s timeout is elapsed"
+    BEFORE_AUTH = "Source: Prompt before auth"
+    AUTH = "Source: Authorization done"
+
+
+async def get_watchdog_logger() -> logging.Logger:
     logging.basicConfig(
-        filename="chat_client.log",
+        filename="watchdog.log",
         level=logging.DEBUG,
         format="%(levelname)s:%(name)s:%(message)s",
     )
-    logger = logging.getLogger(logger_status)
+    logger = logging.getLogger("watchdog_logger")
 
     return logger
 
 
-async def update_env_file(key, value):
+async def update_env_file(key: str, value: str) -> None:
     lines = []
 
     async with aiofiles.open(".env", mode="r") as file:
@@ -26,3 +43,46 @@ async def update_env_file(key, value):
 
     async with aiofiles.open(".env", mode="w") as file:
         await file.writelines(lines)
+
+
+async def send_message(writer: asyncio.StreamWriter, message: str) -> None:
+    writer.write(message.encode())
+    await writer.drain()
+
+
+async def update_messages(
+    queues: Dict[str, asyncio.Queue], filepath: str = "minechat_history.txt"
+) -> None:
+    async with aiofiles.open(filepath, mode="r") as handle:
+        queues["messages_queue"].put_nowait(await handle.read())
+
+
+async def save_message(
+    history_queue: asyncio.Queue, message: str, filepath: str = "minechat_history.txt"
+) -> None:
+    async with aiofiles.open(filepath, mode="a") as handle:
+        history_queue.put_nowait(await handle.write(message))
+
+
+async def watch_for_connection(
+    watchdog_queue: asyncio.Queue, status: ConnectionStatus, message: str = None
+) -> None:
+    logger = await get_watchdog_logger()
+    watchdog_queue.put_nowait(await write_log(status, message, logger))
+
+
+async def write_log(
+    connection_status: ConnectionStatus, message: str, logger: logging.Logger
+) -> None:
+    logger.info(f"{time.time()} {connection_status.value} {message}")
+
+
+async def get_queues() -> Dict[str, asyncio.Queue]:
+    queues = {
+        "messages_queue": asyncio.Queue(),
+        "sending_queue": asyncio.Queue(),
+        "status_updates_queue": asyncio.Queue(),
+        "watchdog_queue": asyncio.Queue(),
+    }
+
+    return queues
